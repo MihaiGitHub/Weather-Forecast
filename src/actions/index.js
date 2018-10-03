c637aa80f309c499d926fbec31e15b2598958ce2
MihaiSmarandache <mihai.sanfran@gmail.com>
2018-10-02 21:40:30 -0700

    Create action creator fetchWeather

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,19 @@
-const APIkey = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+import axios from 'axios';
+
+const API_KEY = '194bf6647e4b09b675d344a119cca95c';
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+    const url = `${ROOT_URL}&q=$`;
+
+    // Returns a prommise called request
+    const request = axios.get(url);
+
+    // Action creators need to return an action; an object with a mandatory type key value pair
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
