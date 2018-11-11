0f6b98c3fc9a056398edc4fa9f3f483b55c4299c
MihaiSmarandache <mihai.sanfran@gmail.com>
2018-11-11 10:39:23 -0700

    Update state with new city from fetch weather reducer

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,13 @@
-export default function(state = null, action){
+import { FETCH_WEATHER } from '../actions/index';
+
+// New reducer for handling fetch weather action
+export default function(state = [], action){
+    switch (action.type){
+        case FETCH_WEATHER:
+            // Update state with cities; Take all entries inside state and insert into new array along with action.payload.data
+            return [ action.payload.data, ...state ]; // New array will be [ city, city, city ]
+            
+    }
 
     return state;
 }
