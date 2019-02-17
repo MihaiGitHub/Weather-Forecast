6b0d4b48029ba655aa6dd392cdaae854edf76e34
MihaiSmarandache <mihai.sanfran@gmail.com>
2019-02-16 22:48:39 -0700

    Pull off an array of temperatures from the city data list

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -4,6 +4,10 @@ import { connect } from 'net';
 class WeatherList extends Component {
     renderWeather(cityData){
         const name = cityData.city.name;
+        // Pull off an array of temperatures
+        // Map over the list of all the different forecasts for each city
+        // Inside of map pass a function; All items inside list array will be passed to this function as the argument weather
+        const temps = cityData.list.map(weather => weather.main.temp); // Returns the temperature for each row inside list
 
         <tr key={name}>
             <td>{name}</td>
